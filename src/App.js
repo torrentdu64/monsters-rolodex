af25848b267aa44b30936fa78353ed18266a1f8d
torrent benoit <torrentdu64@gmail.com>
2019-12-14 13:31:50 +1300

    map function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          targegit initt="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankestein'
+        },
+        {
+          name: 'Dracula'
+        },
+        {
+          name: 'Zombie'
+        }
+      ]
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {this.state.monsters.map( (monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>
+        })}
+      </div>
+    );
+  }
+
 }
 
 export default App;
