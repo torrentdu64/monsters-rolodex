05a70dfe26a5bfb16e8c5395e17e4c3b65166ceb
torrent benoit <torrentdu64@gmail.com>
2019-12-15 09:24:11 +1300

    Updates

---
+++ precache-manifest.40fef550e4649ac4856f4f3688631216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56783e761a647d5d7702b2bbaaa1e3e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "83f3be81db8712ccd65c",
+    "url": "/monster-rolodex/static/css/main.59dd8ecd.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83f3be81db8712ccd65c",
+    "url": "/monster-rolodex/static/js/main.d89ce4cd.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
