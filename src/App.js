f79a66d7304f151bfb3986fd5face3c4ea15e51c
torrent benoit <torrentdu64@gmail.com>
2019-12-15 09:25:58 +1300

    test

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
-import CardList from '../src/components/card-list/card-list.component';
+import CardList  from './components/card-list/card-list.component';
+import  SearchBox  from './components/search-bar/search-bar.component';
 
 import './App.css';
-import SearchBar from '../src/components/search-bar/search-bar.component';
 
-class  App extends Component {
+class App extends Component {
+  constructor() {
+    super();
 
-  constructor(props){
-    super(props);
     this.state = {
       monsters: [],
       searchField: ''
-    }
-  }
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(user => this.setState({monsters: user}))
+    };
   }
 
-  handleChange = (e) => {
-    this.setState({searchField: e.target.value })
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
   };
- 
-  render(){
-    const {monsters, searchField} = this.state;
-    const filterMonsters = monsters.filter( (monster) => {
-      return monster.name.toLowerCase().includes(searchField.toLowerCase())
-    });
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
-      <div className="App">
-      <h1> Monsters Rolodex</h1>
-      <SearchBar 
-      placeholder="search monster"
-      handleChange={this.handleChange}
-      />
-        <CardList monsters={filterMonsters} />
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
-
 }
 
 export default App;
